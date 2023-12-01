9bb755a4f86f58fb18da26ac56fe030d60e28039
James Newton <blackslate@lexogram.com>
2023-12-01 22:45:13 +0300

    Use async/await instead of promises
    script.js
    ---
    * Wrap code for fetching Pokemon names in an async IIFE
    * Use await to assign response and data variables
    * Generate value of 'names' directly from data.results.map()
    * Set pokemon.innerHTML directly in generateOptions()
    * Unwrap the lines for choosing Pikachu by default (no special function)
    
    * Make the getPokemonData function async
    * Use await to assign response and data variables
    * Omit the step of filtering out abilities, stats, sprites and name from data

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const pokemon = document.getElementById("pokemon")
 const display = document.getElementById("display")
 
 
+// Make the select element interactive
 pokemon.addEventListener("change", getPokemonData)
 
 
@@ -10,65 +11,63 @@ const baseURL = "https://pokeapi.co/api/v2/pokemon/"
 const all = "?limit=811"
 const urlForAllPokemon = baseURL+all
 
-// Populate the <select> element with options containing the
-// names of all possible pokemons
-fetch(urlForAllPokemon)               // data stream as promise
-.then(result => date = result.json()) // final value of stream
-.then(json => json.results)           // array of objects
-// Extract just the name from each object
-.then(results => results.map( object => object.name ))
-// Generate a template string of option elements
-.then(generateOptions)
-// Apply the option elements to the select element
-.then(options => pokemon.innerHTML = options)
-// Select Pikachu by default
-.then(choosePikachu)
+
+// Wrap the code that fetches all the Pokemon names in an
+// Immediately Invoked Function Expression (IIFE) so that it ca
+// be declared as async. This allows us to use the await syntactic
+// soap for promises inside it.
+
+;(async function (){
+  // Populate the <select> element with options containing the
+  // names of all possible pokemons
+  const response = await fetch(urlForAllPokemon)
+  const data = await response.json()
+  const names = data.results.map( object => object.name )
+  generateOptions(names)
 
 
+  // Convert an array of names to a template string representing
+  // a series of <option> elements, and populate the <select>
+  // element with them
+  function generateOptions(names) {
+    // Order alphabetically
+    names = names.sort()
 
-// Convert an array of names to a template string representing
-// a series of <li> elements
-function generateOptions(names) {
-  // Order alphebetically
-  names = names.sort()
+    const options = names.reduce(( options, name ) => {
+      // Capitalize the name for display in the select element
+      const Name = name[0].toUpperCase() + name.slice(1)
 
-  const options = names.reduce(( options, name ) => {
-    // Capitalize the name for display in the select element
-    const Name = name[0].toUpperCase() + name.slice(1)
+      options += `
+      <option value="${name}">${Name}</option>`
 
-    options += `
-    <option value="${name}">${Name}</option>`
+      return options
+    }, "")
 
-    return options
-  }, "")
-
-  return options
-}
+    pokemon.innerHTML = options
+  }
 
 
-// Set Pikachu as the default Pokemon immediately after launch
-function choosePikachu() {
+  // Choose Pikachu by default
   pokemon.value = "pikachu"
   getPokemonData()
-}
+})()
 
+
+/*
+ Other functions can be outside the scope of the IIFE, because
+ they will not be called until the select element is ready.
+*/
 
 
 // Fetch the data for the selected pokemon, and display it as a
-// card
-function getPokemonData() {
+// card.
+async function getPokemonData() {
   const url = baseURL + pokemon.value
 
- fetch(url)                      // data stream as a promise
-  .then(result => result.json()) // final value of stream
-  // Extract the values of the properties we need to display
-  .then(data => {
-    const { abilities, stats, sprites, name } = data
-    return { abilities, stats, sprites, name }
-  })
-  .then(createCard)
+  const response = await fetch(url)
+  const data = await response.json()
+  createCard(data)
 }
-
 
 
 function createCard ({ abilities, stats, sprites, name }) {
@@ -93,13 +92,13 @@ function createCard ({ abilities, stats, sprites, name }) {
     return list
   }, "<ul>") + "</ul>"
 
-
- display.innerHTML = `
-   <h2>${name}</h2>
-   <img src="${src}" alt="${name}">
-   <h3>stats</h3>
-   ${statList}
-   <h3>abilities</h3>
-   ${abilityList}
- `
+  // Show the card
+  display.innerHTML = `
+    <h2>${name}</h2>
+    <img src="${src}" alt="${name}">
+    <h3>stats</h3>
+    ${statList}
+    <h3>abilities</h3>
+    ${abilityList}
+  `
 }
