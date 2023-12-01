ad50623616e8fcae446d238f5afd28a2c6036297
James Newton <blackslate@lexogram.com>
2023-12-01 22:10:31 +0300

    Simplify UI; Refactor JavaScript; Add CSS
    index.html
    ---
    - Remove unnecessary Find button
    
    script.js
    ---
    * Remove 'find' constant and its attribution of an event listener
    * Remove 'const data =' from fetch call with promise
    * Remove author-time console logging
    * Add comprehensive comments
    * Add separate generateOptions function and call it with thenable
    * Add choosePikachu function and call it when options are populated
    * Read 'name' for current Pokemon from the data fetched for it
    * createCard
      + Destructure object passed to createCard directly in the parameters
      + Reorder creation of constants to match order in final template string
      + Generate `<ul>` elements directly from the abilities and stats arrays, without creating an intermediate array of filtered data
      + Rename 'image' const to 'src'
    
    styles.css
    ---
    * Add all the rule sets needed to display Pokemen card as expected

--- script.js
+++ script.js
@@ -1,97 +1,102 @@
 const pokemon = document.getElementById("pokemon")
-const find = document.getElementById("find")
 const display = document.getElementById("display")
 
 
-pokemon.addEventListener("change",getPokemonData )
+pokemon.addEventListener("change", getPokemonData)
 
+
+// Prepare to use the API
 const baseURL = "https://pokeapi.co/api/v2/pokemon/"
 const all = "?limit=811"
-
 const urlForAllPokemon = baseURL+all
 
-const data = fetch(urlForAllPokemon)
-.then(result => date = result.json())
-.then(json => json.results)
+// Populate the <select> element with options containing the
+// names of all possible pokemons
+fetch(urlForAllPokemon)               // data stream as promise
+.then(result => date = result.json()) // final value of stream
+.then(json => json.results)           // array of objects
+// Extract just the name from each object
 .then(results => results.map( object => object.name ))
-.then(names => names.reduce(( options, name ) => {
-  options += `
-  <option value="${name}">${name}</option>`
-  return options
-}, ""))
+// Generate a template string of option elements
+.then(generateOptions)
+// Apply the option elements to the select element
 .then(options => pokemon.innerHTML = options)
+// Select Pikachu by default
+.then(choosePikachu)
 
 
-find.addEventListener("click", getPokemonData)
 
-function getPokemonData(event) {
-  // console.log("event:", event);
-  const name = pokemon.value
-  console.log("pokemonToFind:", name);
-  const url = baseURL + name
+// Convert an array of names to a template string representing
+// a series of <li> elements
+function generateOptions(names) {
+  // Order alphebetically
+  names = names.sort()
 
- fetch(url)
-  .then(result => result.json())
-  .then(data => {
-    const { abilities, stats, sprites } = data
-    return { abilities, stats, sprites, name }
-  })
-  .then(createTicket)
-  
+  const options = names.reduce(( options, name ) => {
+    // Capitalize the name for display in the select element
+    const Name = name[0].toUpperCase() + name.slice(1)
+
+    options += `
+    <option value="${name}">${Name}</option>`
+
+    return options
+  }, "")
+
+  return options
 }
 
 
-function createTicket ( object ) {
-  console.log("object:", object)
- const { abilities, stats, sprites, name } =  object
+// Set Pikachu as the default Pokemon immediately after launch
+function choosePikachu() {
+  pokemon.value = "pikachu"
+  getPokemonData()
+}
 
-// ability: Object { name: "overgrow", url: "https://pokeapi.co/api/v2/ability/65/" }
-// ​​​​
-// name: "overgrow"
-// ​​​​
-// url: "https://pokeapi.co/api/v2/ability/65/"
-// ​​​​
-// <prototype>: Object { … }
-// ​​​
-// is_hidden: false
-// ​​​
-// slot: 1
-// ​​​
-// <prototype>: Object { … }
- const abilityNames = abilities.map( object => object.ability.name)
 
- const statArray = stats.map( object => {
-   return {
-     name: object.stat.name,
-     value: object.base_stat
-   }
- })
 
- 
- const image = sprites.front_default
+// Fetch the data for the selected pokemon, and display it as a
+// card
+function getPokemonData() {
+  const url = baseURL + pokemon.value
 
- console.log("abilityNames:", abilityNames);
- console.log("statArray:", statArray);
- console.log("image:", image);
+ fetch(url)                      // data stream as a promise
+  .then(result => result.json()) // final value of stream
+  // Extract the values of the properties we need to display
+  .then(data => {
+    const { abilities, stats, sprites, name } = data
+    return { abilities, stats, sprites, name }
+  })
+  .then(createCard)
+}
 
- const statList = statArray.reduce(( list, item ) => {
-   list += `<li><span>${item.name}</span><span>${item.value}</span></li>
-   `
-   return list
- }, "<ul>") + "</ul>"
-console.log("statList:", statList);
 
-const abilityList = abilityNames.reduce(( list, ability ) => {
-  list += `<li>${ability}</li>
-  `
-  return list
-}, "<ul>") + "</ul>"
-console.log("statList:", statList);
+
+function createCard ({ abilities, stats, sprites, name }) {
+  // Choose the default image URL
+  const src = sprites.front_default
+
+  // Generate a <ul> element from the names and base_stat values
+  // of the stats
+  const statList = stats.reduce(( list, object ) => {
+    list += `
+    <li>
+      <span>${object.stat.name}</span>
+      <span>${object.base_stat}</span>
+    </li>`
+    return list
+  }, "<ul id='stats'>") + "</ul>"
+
+  // Generate a <ul> element from the ability names
+  const abilityList = abilities.reduce(( list, object ) => {
+    list += `<li>${object.ability.name}</li>
+    `
+    return list
+  }, "<ul>") + "</ul>"
 
 
  display.innerHTML = `
    <h2>${name}</h2>
-   <img src="${image}" alt="${name}">
+   <img src="${src}" alt="${name}">
    <h3>stats</h3>
    ${statList}
    <h3>abilities</h3>
